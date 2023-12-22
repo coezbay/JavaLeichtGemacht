44894505aa5f859af3bde8c1b3d5e04bc381216f
coezbay <info@coezbay.com>
2023-12-22 17:20:58 +0100

    Variablen

---
+++ src/main/java/OperatorenI.java
@@ -0,0 +1,34 @@
+
+public class OperatorenI {
+
+    public static void main(String[] args) {
+    //DingDong
+
+        // Was sind Operatoren?
+        /*
+         * Mit Operatoren kann man Variablen verändern oder allgemein
+         * mit ihnen arbeiten
+         *
+         */
+
+        int varZahl = 5;
+
+        int zahl1 = 10;
+        int zahl2 = 5;
+
+        int ergebnis;
+
+        ergebnis = zahl1 + zahl2; // 15
+        System.out.println(ergebnis);
+
+        ergebnis = zahl1 - zahl2; // 5
+        System.out.println(ergebnis);
+
+        ergebnis = zahl1 * zahl2; // 50
+        System.out.println(ergebnis);
+
+        ergebnis = zahl1 / zahl2; // 2
+        System.out.println(ergebnis);
+    }
+
+}
