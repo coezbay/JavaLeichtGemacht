aeb4614f35a39e2f8e7672d4fbe78d473ec92b3a
coezbay <info@coezbay.com>
2023-12-22 17:01:34 +0100

    Test

---
+++ src/main/java/Zeichenkette.java
@@ -0,0 +1,25 @@
+public class Zeichenkette {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // 8 primitive Datentypen in Java
+        /*
+         * byte
+         * short
+         * int
+         * long
+         * float
+         * double
+         * boolean
+         * char
+         */
+        // <Datentyp> <Variablenamen> = <Wert> ;
+ char varZeichen = '!';
+
+ String varZeichenkette = "Ich bin eine Zeichenkette";
+
+ String name = "Christian";
+ String alter = "25";
+
+ System.out.println("Dein Name ist " + name + " und Du bist " + alter + " Jahre alt.");
+    }
+}
---
+++ src/main/java/intFloatDouble.java
@@ -0,0 +1,13 @@
+public class intFloatDouble {
+
+    public static void main(String[] args) {
+
+        int varZahl1 = 10 / 3;
+        float varZahl2 = 10f / 3f;
+        double varZahl3 = 10d / 3d;
+
+        System.out.println("int " + varZahl1);
+        System.out.println("float " + varZahl2);
+        System.out.println("double " + varZahl3);
+    }
+}
