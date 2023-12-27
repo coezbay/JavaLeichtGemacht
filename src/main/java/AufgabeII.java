9344e60f29e5189e826d8dfe60c06c1f867f7de5
coezbay <info@coezbay.com>
2023-12-27 18:17:10 +0100

    Übung I + II

---
+++ src/main/java/AufgabeII.java
@@ -0,0 +1,19 @@
+
+public class AufgabeII {
+
+    public static void main(String[] args) {
+        //DingoDongo
+
+        /*
+        * Aufgabe
+        *
+        * 1. Auto kostet: 3500
+        * 2. Auto kostet: 2340,48
+        *
+        * 1. Daten in Variablen abspeichern
+        * 2. Kommazahl umwandeln zu einer Ganzzahl
+        * 3. Beide Ganzzahlen zu einem Gesamtbetrag verrechnen
+        * 4. Gesamtbetrag ausgeben mit Hilfe von System.out.println("");
+         */
+    }
+}
