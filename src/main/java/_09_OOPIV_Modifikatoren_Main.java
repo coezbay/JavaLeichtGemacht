64f61a275040dcd06b71a4a32a2f1c141125063b
coezbay <info@coezbay.com>
2024-01-18 14:50:10 +0100

    OOP IV Zugriffsmodifikatoren

--- src/main/java/_09_OOPIV_Modifikatoren_Main.java
+++ src/main/java/_09_OOPIV_Modifikatoren_Main.java
@@ -3,6 +3,21 @@ public class _09_OOPIV_Modifikatoren_Main {
     public static void main(String[] args) {
         // Dingo Dongo
 
+//| Zugriffsmodifikatoren:
+//|                    | public | protected | (package-private) | private |
+//|--------------------|--------|-----------|-------------------|---------|
+//| Eigene Klasse      | Ja     | Ja        | Ja                | Ja      |
+//| Gleiches Package   | Ja     | Ja        | Ja                | Nein    |
+//| Subklasse          | Ja     | Ja        | Nein              | Nein    |
+//| Fremde Klasse      | Ja     | Nein      | Nein              | Nein    |
+
+//        In dieser Tabelle:
+//
+//        Eigene Klasse: Zeigt die Zugriffsmöglichkeiten innerhalb der eigenen Klasse.
+//        Gleiches Package: Bezieht sich auf Klassen, die sich im selben Package befinden.
+//        Subklasse: Zeigt den Zugriff in Subklassen (abgeleiteten Klassen), sowohl im gleichen als auch in anderen Packages.
+//        Fremde Klasse: Bezieht sich auf Klassen in anderen Packages, die nicht Subklassen sind.
+
 //        _09_OOPIV_Modifikatoren_Held player1 = new _09_OOPIV_Modifikatoren_Held();
 //        player1.lebenspunkte = 100;
 //        System.out.println("Der Held player1 hat: " + player1.lebenspunkte + " Lebenspunkte.");
