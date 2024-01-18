a71160dcb1750ac240e5940d8c1c14e540cae163
coezbay <info@coezbay.com>
2024-01-18 05:53:55 +0100

    Array

---
+++ src/main/java/_05_Array_WasIstEinArray.java
@@ -0,0 +1,17 @@
+public class _05_Array_WasIstEinArray {
+    public static void main(String[] args) {
+        // Dingo Dongo
+
+        // Veriable Deklaration
+        // Datentyp Name = Wert;
+        int zahl = 50;
+
+        // Array Deklaration
+        // Datentyp[] Name = new Datentyp[Anzahl];
+        int[] zahlenArray = new int[7];
+        zahlenArray[3] = 50;
+        zahlenArray[5] = 500;
+        zahlenArray[0] = 40;
+    }
+
+}
