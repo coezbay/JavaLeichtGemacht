e775077a4603dfc62979d4d00e6231cf8df2f524
coezbay <info@coezbay.com>
2024-01-18 14:15:47 +0100

    OOP IV Public Private

---
+++ src/main/java/_09_OOPIV_Modifikatoren_Held.java
@@ -0,0 +1,25 @@
+public class _09_OOPIV_Modifikatoren_Held {
+
+    // Eigenschaften / Attribute
+    public String name;
+    private int lebenspunkte;
+
+    // Konstruktor
+
+    public _09_OOPIV_Modifikatoren_Held(int lebenspunkte) {
+        if (lebenspunkte <= 100 && lebenspunkte > 0) {
+            this.lebenspunkte = lebenspunkte;
+        }else{
+            this.lebenspunkte = 90;
+        }
+    }
+
+    // Methoden
+    public void getLebenspunkte() {
+        System.out.println(this.lebenspunkte);
+    }
+
+    public void setLebenspunkte(){
+
+    }
+}
--- src/main/java/_09_OOPIV_Modifikatoren_Main.java
+++ src/main/java/_09_OOPIV_Modifikatoren_Main.java
@@ -1,5 +1,18 @@
 public class _09_OOPIV_Modifikatoren_Main {
 
     public static void main(String[] args) {
+        // Dingo Dongo
+
+//        _09_OOPIV_Modifikatoren_Held player1 = new _09_OOPIV_Modifikatoren_Held();
+//        player1.lebenspunkte = 100;
+//        System.out.println("Der Held player1 hat: " + player1.lebenspunkte + " Lebenspunkte.");
+//
+//        _09_OOPIV_Modifikatoren_Held player2 = new _09_OOPIV_Modifikatoren_Held();
+//        player2.lebenspunkte = -100;
+//        System.out.println("Der Held player2 hat: " + player2.lebenspunkte + " Lebenspunkte.");
+
+        _09_OOPIV_Modifikatoren_Held player1 = new _09_OOPIV_Modifikatoren_Held(46);
+        player1.getLebenspunkte();
+
     }
 }
