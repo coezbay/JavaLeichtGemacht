4a0bcdfeb7d071030d701bacbe2a467fa514e3bf
coezbay <info@coezbay.com>
2024-02-08 00:05:32 +0100

    A_05_Grundlagen_ZeichenBool optimiert

--- src/main/java/A_05_Grundlagen_ZeichenBool.java
+++ src/main/java/A_05_Grundlagen_ZeichenBool.java
@@ -1,10 +1,18 @@
 public class A_05_Grundlagen_ZeichenBool {
 
     public static void main(String[] args) {
-        char varZeichen = '\u00A9';
-        System.out.println("Das Bild unterliegt dem " + varZeichen);
+        // Deklaration und Initialisierung einer char-Variablen mit dem Copyright-Zeichen
+        char varZeichen = '©';
+        System.out.println("Das Bild unterliegt dem " + varZeichen + "-Zeichen");
 
-        boolean userKlick = true;
+        // Deklaration und Initialisierung einer boolean-Variablen
+        boolean userKlick = true; // Angenommen, der Benutzer hat geklickt
 
+        // Verwendung der boolean-Variable in einer bedingten Anweisung
+        if (userKlick) {
+            System.out.println("Der Benutzer hat geklickt.");
+        } else {
+            System.out.println("Der Benutzer hat nicht geklickt.");
+        }
     }
 }
